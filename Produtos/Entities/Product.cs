84ac51e236adc6226d0a62d76e038f5a8ccbabfa
Alexandre Rogério <xandaosilva93@hotmail.com>
2021-04-30 09:45:06 -0300

    Add CalculationService and Program

---
+++ Produtos/Entities/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Produtos.Entities
+{
+    class Product : IComparable
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        public String Name { get; set; }
+        public Double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CI);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Product))
+            {
+                throw new ArgumentException("Comparing error: argument is not a Product");
+            }
+
+            Product other = obj as Product;
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
--- Produtos/Program.cs
+++ Produtos/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Produtos.Entities;
+using Produtos.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace Produtos
 {
@@ -6,7 +10,25 @@ namespace Produtos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CI);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Most expansive: ");
+            Console.WriteLine(max);
         }
     }
 }
